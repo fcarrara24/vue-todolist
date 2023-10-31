7e10d0a71537ffabdc7d828d5aaf45e8f23014ad
fcarrara24 <f.carrara24@studenti.unibg.it>
2023-10-31 15:51:43 +0100

    milestone 1

--- js/script.js
+++ js/script.js
@@ -2,12 +2,31 @@ const { createApp } = Vue;
 const obj = {
     data() {
         return {
-            message: 'hello vue'
+            tasks: [
+                {
+                    id: 1,
+                    text: 'prendere i diamanti',
+                    done: false
+                },
+                {
+                    id: 2,
+                    text: 'fare a',
+                    done: false
+                },
+                {
+                    id: 3,
+                    text: 'fare il bucato',
+                    done: false
+                },
+            ],
+            lastId: 3,
+            todoText: '',
+            filterValue: '',
         }
     },
     methods: {
-        changeMessage() {
-            this.message = 'ciao';
+        changeThings() {
+
         }
     }
 }
